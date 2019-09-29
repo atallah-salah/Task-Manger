01315b9739fceed5f82126e40ed51bcc570a4f2f
atallah-salah <atallah_salah@outlook.com>
2019-09-29 17:21:49 +0300

    handle tasks errors

--- client-side/src/app/components/task-view/task-view.component.ts
+++ client-side/src/app/components/task-view/task-view.component.ts
@@ -16,11 +16,14 @@ export class TaskViewComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe((params:Params)=>{
+      if(params.listId){
         this.taskService.getTasks(params.listId).subscribe((tasks:Task[])=>{
           this.tasks = tasks;
         })
+      }else{
+        this.tasks = undefined;
       }
-    )
+    })
     
     this.taskService.getLists().subscribe((lists:List[])=>{
       this.lists = lists;
